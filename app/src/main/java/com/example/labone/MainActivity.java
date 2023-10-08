cb42fef9a8a11fdfdc77bda83c45ee558f44c5d2
Domas Sestakovas <sestakovasdomas@gmail.com>
2023-10-08 21:16:07 +0200

    button now changes the text

--- app/src/main/java/com/example/labone/MainActivity.java
+++ app/src/main/java/com/example/labone/MainActivity.java
@@ -3,12 +3,26 @@
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import org.w3c.dom.Text;
+
+
+ public class MainActivity extends AppCompatActivity {
+
+    TextView textMain;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.textMain = findViewById(R.id.textMain);
+    }
+
+    public void onButtonChangeTextClick(View view) {
+        this.textMain.setText("Kodel?");
     }
 }
