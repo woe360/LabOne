369501a46e290a33006ccdd279026abc2f48b052
Domas Sestakovas <sestakovasdomas@gmail.com>
2023-10-09 11:37:31 +0200

    button changes color

--- app/src/main/java/com/example/labone/MainActivity.java
+++ app/src/main/java/com/example/labone/MainActivity.java
@@ -2,9 +2,13 @@
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.Button;
+
 
 import org.w3c.dom.Text;
 
@@ -12,7 +16,8 @@ import org.w3c.dom.Text;
  public class MainActivity extends AppCompatActivity {
 
     TextView textMain;
-
+    Button buttonChangeText;
+    Button buttonChangeColor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +25,20 @@ import org.w3c.dom.Text;
         setContentView(R.layout.activity_main);
 
         this.textMain = findViewById(R.id.textMain);
+        this.buttonChangeText = findViewById(R.id.buttonChangeText);
+        this.buttonChangeColor = findViewById(R.id.buttonChangeColor);
+        buttonChangeText.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"Some message", Toast.LENGTH_LONG).show();
+            }
+
+        });
+        buttonChangeColor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textMain.setTextColor(Color.RED);
+            }
+        });
     }
 
     public void onButtonChangeTextClick(View view) {
